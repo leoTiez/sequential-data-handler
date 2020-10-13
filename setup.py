da7190cd3df97c7ca2b0aa0c4844da13f5cf0109
Leo Zeitler <leozeitler95@gmail.com>
2020-10-13 15:26:58 +0200

    Update: Added setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import setuptools
+
+setuptools.setup(
+    name="data-handler-leoTiez",
+    version="0.0.1",
+    author="Leo Zeitler",
+    author_email="leo.zeitler@i2bc.paris-saclay.fr",
+    description="Data handler for managing bigwig and bed files and their respective data",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: CECILL License"
+    ],
+    python_requires='>=3.6',
+)
+
