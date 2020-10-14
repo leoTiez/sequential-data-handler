773ff403bbc2c7a239cf07383af4ce99ba0b2985
Leo Zeitler <leozeitler95@gmail.com>
2020-10-14 15:31:21 +0200

    Update: Added get values function w/out normalising data

--- seqDataHandler.py
+++ seqDataHandler.py
@@ -41,15 +41,13 @@ def load_big_file(name, rel_path='data'):
     return file
 
 
-def normalise_over_annotation(bw_list, bed_ref, smoothing=None):
+def annotation(bw_list):
     if len(bw_list) == 0:
         raise ValueError('List with bigwig objects must not be empty.')
 
     all_values = []
-    bw_gen_mapping = []
     for _ in bw_list:
         all_values.append([])
-        bw_gen_mapping.append([])
 
     counter = 0
     chrom_start = {}
@@ -63,6 +61,13 @@ def normalise_over_annotation(bw_list, bed_ref, smoothing=None):
         for num, bw in enumerate(bw_list):
             values = np.asarray(bw.values('chr%s' % roman_num, 0, length))
             all_values[num].extend(np.nan_to_num(values, nan=0.0).tolist())
+
+    return all_values, chrom_start
+
+
+def normalise_over_annotation(bw_list, bed_ref, smoothing=None):
+    all_values, chrom_start = annotation(bw_list=bw_list)
+    bw_gen_mapping = [[] for _ in all_values]
 
     means = []
     stds = []
