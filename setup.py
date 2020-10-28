bb1d52e7f6c1e5faf7373a5f60e253fdd1bb5f06
Leo Zeitler <leozeitler95@gmail.com>
2020-10-28 08:48:45 +0100

    Bugfix: Added missing install requirements

--- setup.py
+++ setup.py
@@ -9,6 +9,13 @@ setuptools.setup(
     description="Data handler for managing bigwig and bed files and their respective data",
     url="git@github.com:leoTiez/seqDataHandler.git",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'roman>=3.3',
+        'numpy>=1.18',
+        'scipy>=1.5',
+        'pyBigWig>=0.3',
+        'pybedtools>=0.8'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
