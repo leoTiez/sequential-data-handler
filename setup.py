a1e85283e45aec74c37586964ccc61147022adc4
Leo Zeitler <leozeitler95@gmail.com>
2020-11-10 13:13:52 +0100

    Update: Update version to include a new feature
    Switching the normalisation method is seen as a new feature.
    From now onwards, we will be more consistent in the versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="seqDataHandler",
-    version="0.0.5",
+    version="0.1.0",
     author="Leo Zeitler",
     author_email="leo.zeitler@i2bc.paris-saclay.fr",
     description="Data handler for managing bigwig and bed files and their respective data",
